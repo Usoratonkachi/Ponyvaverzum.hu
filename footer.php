0ed256c65378ca472705ee783e20fa87d2f01994
Usoratonkachi <164550582+Usoratonkachi@users.noreply.github.com>
2024-05-08 12:15:57 +0200

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
-<footer class="footer">
+ <div><p>Ügyvezető: <strong>Rejtély Elemér</strong> &nbsp
+    E-mail: <a href="mailto:ugyfelszolgalat@ponyvaverzum.hu">ugyfelszolgalat@ponyvaverzum.hu</a></P></div>
         <?php if(isset($lablec['copyright'])) { ?>&copy;&nbsp;<?= $lablec['copyright'] ?> <?php } ?>
 		&nbsp;
         <?php if(isset($lablec['ceg'])) { ?><?= $lablec['ceg']; ?><?php } ?>
-    </footer>
