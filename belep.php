0bbd57bef4f1b293228530762991c699dea09ab9
Usoratonkachi <164550582+Usoratonkachi@users.noreply.github.com>
2024-05-08 12:08:32 +0200

    Update belep.php

--- belep.php
+++ belep.php
@@ -2,17 +2,25 @@
 if(isset($_POST['felhasznalo']) && isset($_POST['jelszo'])) {
     try {
         // Kapcsolódás
-        $dbh = new PDO('mysql:host=localhost;dbname=gyakorlat7', 'root', '',
+        $dbh = new PDO('mysql:host=localhost;dbname=konyvek', 'konyvek', '123456',
                         array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // Emuláció kikapcsolása
+        
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
         
         // Felhsználó keresése
         $sqlSelect = "select id, csaladi_nev, uto_nev from felhasznalok where bejelentkezes = :bejelentkezes and jelszo = sha1(:jelszo)";
         $sth = $dbh->prepare($sqlSelect);
-        $sth->execute(array(':bejelentkezes' => $_POST['felhasznalo'], ':jelszo' => $_POST['jelszo']));
+        $sth->bindParam(':bejelentkezes', $_POST['felhasznalo'], PDO::PARAM_STR);
+        $sth->bindParam(':jelszo', $_POST['jelszo'], PDO::PARAM_STR);
+        $sth->execute();
+        
         $row = $sth->fetch(PDO::FETCH_ASSOC);
         if($row) {
-            $_SESSION['csn'] = $row['csaladi_nev']; $_SESSION['un'] = $row['uto_nev']; $_SESSION['login'] = $_POST['felhasznalo'];
+            session_start();
+            $_SESSION['csn'] = $row['csaladi_nev']; 
+            $_SESSION['un'] = $row['uto_nev']; 
+            $_SESSION['login'] = $_POST['felhasznalo'];
         }
     }
     catch (PDOException $e) {
